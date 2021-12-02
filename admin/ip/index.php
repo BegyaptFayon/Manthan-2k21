6c7ab3ef2ea9076cd7002e8b4848a25e61ee8108
BegyaptFayon <shubhanshu.bhagat.8@gmail.com>
2021-12-03 01:24:09 +0530

    added ip page

---
+++ admin/ip/index.php
@@ -0,0 +1,33 @@
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="login.css">
+    <title>Track My IP</title>
+</head>
+
+<body>
+    <div class="header">
+        <h2>Cyber-Dominators</h2>
+    </div>
+
+    <form method="post" action="">
+        <div class="input-group">
+            <label>Enter IP:</label>
+            <input type="text" name="username" id="username">
+        </div>
+        <div class="input-group">
+            <button type="submit" class="btn" name="login_user">TRACK</button>
+        </div>
+        <div>
+            Tracked IP's <a href="">Click to view the logs</a>
+
+        </div>
+
+    </form>
+</body>
+
+</html>
