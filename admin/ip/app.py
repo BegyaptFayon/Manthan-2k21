58290c24b04803dc764af2435f10d0b34ab59e97
Divy Bhanushali <bhanushalidivy1@gmail.com>
2021-12-07 23:35:44 +0530

    added trigger.py

--- admin/ip/app.py
+++ admin/ip/app.py
@@ -5,14 +5,14 @@ import flask
 app = Flask(__name__)
 
 file = open(
-    'C:\\Users\\shubh\\Documents\\MyProjects\\manthan\\Manthan-2k21\\admin\\ip\\nmap.txt', 'r')
+    'nmap.txt', 'r')
 content = file.readlines()
 
 
 @app.route('/admin/ip', methods=['GET'])
 def get_ip():
 
-    return render_template('template.html', len=len(content), content=content)
+    return render_template('index.php', len=len(content), content=content)
     # return render_template('template.html', content=content, mimetype='text/plain')
     # return jsonify({'data': content})
 
