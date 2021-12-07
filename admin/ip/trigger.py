f3fe9f0cc63d1ff121caabedcbada4436219f72d
BegyaptFayon <shubhanshu.bhagat.8@gmail.com>
2021-12-08 01:32:59 +0530

    partially working

--- admin/ip/trigger.py
+++ admin/ip/trigger.py
@@ -1,15 +1,27 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 app = Flask(__name__)
+
 
 @app.route('/')
 def index():
-  return render_template('index.html')
+    return render_template('index.html')
 
-@app.route('/my-link/')
-def my_link():
-  print ('Fuck off!')
 
-  return 'IP Processed!'
+# @app.route('/my-link/')
+# def my_link():
+#     print('Fuck off!')
+
+#     return 'IP Processed!'
+
+
+@app.route('/my-data/', methods=["GET", "POST"])
+def get_ip():
+    if request.method == "POST":
+        ip = request.form.get("ip")
+        return "Your IP is: " + ip
+    # return render_template("index.html")
+    return "divy"
+
 
 if __name__ == '__main__':
-  app.run(debug=True)
+    app.run(debug=True)
